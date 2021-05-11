b0ae191ff9a0ee2f761f8cd34200bb8d073baa5b
IKENNA <ikenna@IKENNAs-MacBook-Pro.local>
2021-05-11 10:58:03 +0100

    Completed app with assets for bg and icon

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,5 +1,9 @@
+import 'package:brewcrew/models/user.dart';
+import 'package:brewcrew/services/database.dart';
 import 'package:brewcrew/shared/constants.dart';
+import 'package:brewcrew/shared/loading.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingsFormState extends StatefulWidget {
   @override
@@ -17,60 +21,80 @@ class _SettingsFormStateState extends State<SettingsFormState> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          Text(
-            'Update your brew settings.',
-            style: TextStyle(fontSize: 18.0),
-          ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            decoration: textInputDecoration,
-            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
-            onChanged: (val) => setState(() => _currentName = val),
-          ),
-          SizedBox(height: 20.0),
-          DropdownButtonFormField(
-            decoration: textInputDecoration,
-            value: _currentSugars ?? '0',
-              items: sugars.map((sugar) {
-                return DropdownMenuItem(
-                  value: sugar,
-                  child: Text('$sugar sugars'),
-                );
-              }).toList(),
-              onChanged: (val) => setState(() => _currentSugars = val),
-          ),
-          SizedBox(height: 20.0),
-          Text('$_currentStrength'),
-          Slider(
-              value: (_currentStrength ?? 100).toDouble(),
-              onChanged: (val) => setState(() => _currentStrength = val.round()),
-              min: 100,
-              max: 900,
-              divisions: 8,
-              activeColor: Colors.brown[_currentStrength ?? 100],
-              inactiveColor: Colors.brown[_currentStrength ?? 100],
-          ),
-          SizedBox(height: 20.0),
-          ElevatedButton(
-              onPressed: () async {
-                print(_currentName);
-                print(_currentSugars);
-                print(_currentStrength);
-              },
-              child: Text(
-                'Update',
-                style: TextStyle(color: Colors.white),
-              ),
-              style: ElevatedButton.styleFrom(
-              primary: Colors.pink[400]
-              ),
-          )
-        ],
-      ),
+
+    final user = Provider.of<UserDetail>(context);
+
+    return StreamBuilder<UserData>(
+      stream: DatabaseService(uid: user.uid).userData,
+      builder: (context, snapshot) {
+
+        UserData userData = snapshot.data;
+        if (snapshot.hasData) {
+          return Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Update your brew settings.',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  initialValue: userData.name,
+                  decoration: textInputDecoration,
+                  validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+                  onChanged: (val) => setState(() => _currentName = val),
+                ),
+                SizedBox(height: 20.0),
+                DropdownButtonFormField(
+                  decoration: textInputDecoration,
+                  value: _currentSugars ?? userData.sugars,
+                  items: sugars.map((sugar) {
+                    return DropdownMenuItem(
+                      value: sugar,
+                      child: Text('$sugar sugars'),
+                    );
+                  }).toList(),
+                  onChanged: (val) => setState(() => _currentSugars = val),
+                ),
+                SizedBox(height: 20.0),
+                Slider(
+                  value: (_currentStrength ?? userData.strength).toDouble(),
+                  onChanged: (val) => setState(() => _currentStrength = val.round()),
+                  min: 100,
+                  max: 900,
+                  divisions: 8,
+                  activeColor: Colors.brown[_currentStrength ?? 100],
+                  inactiveColor: Colors.brown[_currentStrength ?? 100],
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      await DatabaseService(uid: user.uid).updateUserData(
+                          _currentSugars ?? userData.sugars,
+                          _currentName ?? userData.name,
+                          _currentStrength ?? userData.strength
+                      );
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Text(
+                    'Update',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.pink[400]
+                  ),
+                )
+              ],
+            ),
+          );
+        } else {
+          return Loading();
+        }
+
+      }
     );
   }
 }
